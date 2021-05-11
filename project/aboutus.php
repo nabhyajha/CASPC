86be33aef2a611dae5ee51e5311f4bf7cf8644ac
nabhyajha <66169015+nabhyajha@users.noreply.github.com>
2021-05-11 15:08:46 +0530

    Add files via upload

---
+++ project/aboutus.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset ="UTF-8">
+<meta name="viewpoirt" content="width=device-width,initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<!--Bootstrap CSS-->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+
+<!--Font Awesome  CSS-->
+<link rel="stylesheet" href="css/all.min.css">
+
+<!--Google FONT-->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap" rel="stylesheet">
+
+<!--CUSTOM  CSS-->
+<link rel="stylesheet" href="css/custom.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<title>CASPC</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark pl-5 fixed-top"><!--STart navigation-->
+<a href="" class="navbar-brand">CASPC</a>
+<span class="navbar-text">Customer Happiness and Satisfaction is Our Only Aim</span>
+<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myMenu">
+    <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="myMenu">
+    <ul class="navbar-nav pl-5 custom-nav">
+        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <!-- <li class="nav-item"><a href="registration.php" class="nav-link">Registration</a></li> -->
+        <!-- <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li> -->
+        <li class="nav-item"><a href="contactus.php" class="nav-link">Contact Us</a></li>
+        <li class="nav-item"><a href="aboutus.php" class="nav-link">About Us</a></li>
+
+        <!-- <li class="nav-item"><a href="aboutus.php" class="nav-link">About Us</a></li> -->
+        </ul>
+        </div>
+</div>
+</nav><!--End navigation-->
+
+<!--SERVICES OFFERED-->
+
+  <header class="jumbotron back-image" style="background-image:url(images/logo2.JPG);">
+  
+ 
+<div>
+<div class="container">
+<h1 class="text-center text-white">Major services we offer</h1>
+  
+  <div class="card-columns">
+    <div class="card bg-primary">
+      <div class="card-body text-center">
+      <img src="images/home salon.JPEG" class="img-fluid" style="border-radius:100px" alt="c1">
+         <h4 class="card-title ">Home Salon</h4>
+        <p class="card-text"><ul style="list-style-type:circle"> <li>Trained and specialist beauticians and salonists</li><li>Special attention towards
+         hygiene</li>
+</ul> </p>
+      </div>
+    </div>
+    <div class="card bg-warning">
+      <div class="card-body text-center">
+      <img src="images/pestcontrol.JPEG" class="img-fluid" style="border-radius:100px" alt="c1">
+         <h4 class="card-title ">Pest Control</h4>
+        <p class="card-text"><ul style="list-style-type:circle"><li>100% Eco-friendly chemicals used</li><li>Anti-allergic and non-toxic pesticides used</li></ul></p>
+      </div>
+    </div>
+    <div class="card bg-success">
+      <div class="card-body text-center">
+      <img src="images/appliances.JPEG" class="img-fluid" style="border-radius:100px" alt="c1">
+         <h4 class="card-title ">Appliance Jobs</h4>
+        <p class="card-text"><ul style="list-style-type:circle"><li>Service,Repair,Installation and Uninstallation Jobs</li><li>Experienced and skilled technicians</li></ul></p>
+      </div>
+    </div>
+    <div class="card bg-danger">
+      <div class="card-body text-center">
+      <img src="images/cleaning.JPEG" class="img-fluid" style="border-radius:100px" alt="c1">
+      <h4 class="card-title ">HouseKeeping</h4>
+        <p class="card-text"><ul style="list-style-type:circle"><li>Mechanised equipment and professional cleaning solutions.</li><li>Trained and background verified professionals.</li></ul></p>
+      </div>
+    </div>  
+    <div class="card bg-light">
+      <div class="card-body text-center">
+      <img src="images/housemaintenance.JPEG" class="img-fluid" style="border-radius:100px" alt="c1">
+      <h4 class="card-title ">House Maintenance</h4>
+        <p class="card-text"><ul style="list-style-type:circle"><li>Doorstep repair within 90 mins</li><li>30 days post-service guarantee</li></ul></p>
+      </div>
+    </div>
+  </div>
+  <div>
+</div>
+</body>
+<!--start footer-->
+<footer class="container-fluid bg-dark mt-5 fixed-bottom text-white">
+    <div class="container">
+        <div class="row py-3">
+            <div class="col-md-6">
+                
+</div>
+
+
+
+<div class="col-md-6">
+    <small>Designed by CASPC Designers &copy;2021</small>
+    <small class="ml-2"><a href="login.php">Admin Login</a></small> 
+    </div>
+</div>
+</div>
+
+            
+</div>
+</div>
+</div>
+</footer>
+
+</html>
---
+++ project/index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset ="UTF-8">
+<meta name="viewpoirt" content="width=device-width,initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<!--Bootstrap CSS-->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+
+<!--Font Awesome  CSS-->
+<link rel="stylesheet" href="css/all.min.css">
+
+<!--Google FONT-->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap" rel="stylesheet">
+
+<!--CUSTOM  CSS-->
+<link rel="stylesheet" href="css/custom.css">
+
+<style>
+#div1{ 
+background-image:url(images/logon.JPG); 
+height:600px; 
+width:100%; 
+-webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+ 
+} 
+ 
+#div2{ 
+background-image:url(images/homepage.JPG); 
+height:700px; 
+width:100%; 
+background-size: cover;
+  
+} 
+
+</style>
+
+
+
+<title>CASPC</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm navbar-dark bg-secondary pl-5 fixed-top"><!--STart navigation-->
+<a href="index.php" class="navbar-brand">CASPC</a>
+<span class="navbar-text">Customer Happiness and Satisfaction is Our Only Aim</span>
+<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myMenu">
+    <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="myMenu">
+    <ul class="navbar-nav pl-5 custom-nav">
+        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="registration.php" class="nav-link">Registration</a></li>
+        <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
+        <li class="nav-item"><a href="contactus.php" class="nav-link">Contact Us</a></li>
+        <li class="nav-item"><a href="aboutus.php" class="nav-link">About Us</a></li>
+        </ul>
+        </div>
+
+</nav><!--End navigation-->
+<div id="div1"> 
+  <!-- hi there,it is the first image.  -->
+</div> 
+ 
+<div id="div2"> 
+
+</div> 
+<div class="container p-3 my-3 bg-dark text-white">
+    <div class="jumbotron">
+        <h3 class="text-center">CASPC</h3>
+        <p>there is going to be some big text explaining our website here and lots of stuff lots more lots more lots  more.......</p>
+</div>
+</div><!--End introduction session-->
+
+
+
+<!--Start services section-->
+<div class="container text-center border-bottom-color:#92a8d1 ">
+    <h2>OUR SERVICES</h2>
+    <div class="row mt-4">
+        <div class="col-sm-4 mb-4">
+            <a href="#"><i class="fas fa-graduation-cap fa-8x text-success "></i></a>
+            <h4 class="mt-4">Tution Services</h4>
+</div>
+
+            <div class="col-sm-4 mb-4">
+           <a href="#"><i class="fas fa-sliders-h fa-8x text-primary "></i></a>
+            <h4 class="mt-4">Preventive Maintenance</h4>
+</div>
+<div class="col-sm-4 mb-4">
+            <a href="#"><i class="fas fa-cogs fa-8x text-info "></i></a>
+            <h4 class="mt-4">Fault Repair</h4>
+</div>
+<div class="col-sm-4 mb-4">
+            <a href="#"><i class="fas fa-tv fa-8x text-warning "></i></a>
+            <h4 class="mt-4">Electronic Appliances</h4>
+</div>
+<div class="col-sm-4 mb-4">
+            <a href="#"><i class="fas fa-luggage-cart fa-8x text-dark "></i></a>
+            <h4 class="mt-4">Delivery Services</h4>
+</div>
+<div class="col-sm-4 mb-4">
+            <a href="#"><i class="fas fa-spray-can fa-8x text-secondary "></i></a>
+            <h4 class="mt-4">Cleaning Services</h4>
+</div>
+
+<div class="col-sm-15 mb-4">
+            <a href="#"><i class="fas fa-home fa-8x text-danger "></i></a>
+            <h4 class="mt-4">And Several Other Home Services</h4>
+</div>
+
+
+</div>
+</div><!--End Services Section container-->
+
+
+<!--Start Happy Customers-->
+<div class="jumbotron bg-danger">
+    <div class="container">
+        <h2 class="text-center text-white">Happy Customers</h2>
+        <div class="row mt-5">
+
+            <div class="col-lg-3 col-sm-6">
+                <div class="card shadow-lg mb-2">
+                    <div class="card-body text-center">
+                        <img src="images/c1.JPG" class="img-fluid" style="border-radius:100px" alt="c1">
+                        <h4 class="card-title ">Rahul Gupta</h4>
+                        <p class="card-text">Some feedback of the customers</p>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-3 col-sm-6"><!--CUSTOMER 2-->
+                <div class="card shadow-lg mb-2">
+                    <div class="card-body text-center">
+                        <img src="images/c3.JPG" class="img-fluid" style="border-radius:100px" alt="c3">
+                        <h4 class="card-title ">Rachel Green</h4>
+                        <p class="card-text">Some feedback of the customers</p>
+</div>
+</div>
+</div>
+
+
+
+<div class="col-lg-3 col-sm-6"><!--CUSTOMER 3-->
+                <div class="card shadow-lg mb-2">
+                    <div class="card-body text-center">
+                        <img src="images/c2.JPG" class="img-fluid" style="border-radius:100px" alt="c2">
+                        <h4 class="card-title ">Matt Gosling</h4>
+                        <p class="card-text">Some feedback of the customers</p>
+</div>
+</div>
+</div><!--End happy customer-->
+
+
+
+
+
+<div class="col-lg-3 col-sm-6"><!--CUSTOMER-4-->
+                <div class="card shadow-lg mb-2">
+                    <div class="card-body text-center">
+                        <img src="images/c4.JPG" class="img-fluid" style="border-radius:100px" alt="c4">
+                        <h4 class="card-title ">Priya Kapoor</h4>
+                        <p class="card-text">Some feedback of the customers</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<!--start footer-->
+<footer class="container-fluid bg-dark mt-5 text-white">
+    <div class="container">
+        <div class="row py-3">
+            <div class="col-md-6">
+                <span class="pr-2">Follow Us:</span>
+                <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-facebook-f"></i></a>
+                <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-twitter"></i></a>
+                <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-youtube"></i></a>
+                <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-google-plus-g"></i></a>
+</div>
+
+
+
+<div class="col-md-6">
+    <small>Designed by CASPC Designers &copy;2021</small>
+    <small class="ml-2"><a href="login.php">Admin Login</a></small> 
+    </div>
+</div>
+</div>
+<!--Start of second column-->
+<div style="margin-left:1050px" class="col-md-4 text-center">
+    <strong>Headquarter:</strong><br>
+    CASPC pvt Ltd,<br>
+    New Panvel,Navi Mumbai<br>
+    Maharashtra-410206<br>
+    Phone:9653415547<br>
+    <a href="#" target="_blank">www.CASPC.com</a>
+    <br><br>
+    <strong>Headquarter:</strong><br>
+    CASPC pvt Ltd,<br>
+   Vashi,Navi Mumbai<br>
+    Maharashtra-410203<br>
+    Phone:9653415547<br>
+    <a href="#" target="_blank">www.CASPC.com</a><!--end-->
+            
+</div>
+</div>
+</div>
+</footer>
+
+
+ <!--bootstrap Javascript-->
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/all.min.js"></script>
+
+
+
+
+</body>
+</html>
